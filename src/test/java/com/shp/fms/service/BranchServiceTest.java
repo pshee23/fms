62432561141eeb4b6032867ef7c2ffcb17cfed2b
pshee23 <mabimania23@naver.com>
2023-04-12 01:03:41 +0900

    [UPDATE] Branch Service JUnit Test 작성

--- src/test/java/com/shp/fms/service/BranchServiceTest.java
+++ src/test/java/com/shp/fms/service/BranchServiceTest.java
@@ -1,8 +1,12 @@
 package com.shp.fms.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -34,23 +38,9 @@ class BranchServiceTest {
 	
 	@Test
 	void testRegisterBranch() {
-		BranchRequestBody requestBody = new BranchRequestBody();
-		requestBody.setName("firstName");
-		requestBody.setAddress("firstAddress");
-		requestBody.setPhoneNumber("firstPhoneNumber");
-		
-		Branch firstBranch = new Branch();
-		firstBranch.setName(requestBody.getName());
-		firstBranch.setAddress(requestBody.getAddress());
-		firstBranch.setPhoneNumber(requestBody.getPhoneNumber());
-		
-		BranchInfo firstBranchInfo = BranchInfo.builder()
-				.branchId(1l)
-				.name(firstBranch.getName())
-				.address(firstBranch.getAddress())
-				.phoneNumber(firstBranch.getPhoneNumber())
-				.build();
-		
+		BranchRequestBody requestBody = firstRequestBody();
+		Branch firstBranch = makeBranch(requestBody);
+		BranchInfo firstBranchInfo = makeBranchInfo(firstBranch);
 		
 		// import static org.mockito.Mockito.*; 가 있어야 when 사용가능
 		when(branchMapper.mapToBranch(requestBody)).thenReturn(firstBranch);
@@ -65,32 +55,107 @@ class BranchServiceTest {
 
 	@Test
 	void testModifyBranch() {
-		fail("Not yet implemented");
+		long dummyBranchId = 1l;
+		BranchRequestBody requestBody = firstRequestBody();
+		Branch firstBranch = makeBranch(requestBody);
+		BranchInfo firstBranchInfo = makeBranchInfo(firstBranch);
+		Optional<Branch> opBranch = Optional.of(firstBranch);
+		
+		when(branchRepository.findById(dummyBranchId)).thenReturn(opBranch);
+		when(branchMapper.mapToBranchInfo(firstBranch)).thenReturn(firstBranchInfo);
+		when(branchRepository.save(firstBranch)).thenReturn(firstBranch);
+		when(branchMapper.mapToBranchInfo(firstBranch)).thenReturn(firstBranchInfo);
+		
+		BranchInfo returnInfo = branchService.modifyBranch(dummyBranchId, requestBody);
+		
+		assertEquals(firstBranchInfo, returnInfo);
 	}
 
 	@Test
 	void testDeleteBranch() {
-		fail("Not yet implemented");
+		long dummyBranchId = 1l;
+		
+		when(branchRepository.existsById(any())).thenReturn(true);
+		
+		boolean isDelete = branchService.deleteBranch(dummyBranchId);
+		
+		assertEquals(false, isDelete);
 	}
 
 	@Test
 	void testGetAllBranchInfo() {
-		fail("Not yet implemented");
+		Branch firstBranch = makeBranchWithFirstRequest();
+		BranchInfo firstBranchInfo = makeBranchInfo(firstBranch);
+		List<Branch> branchList = new ArrayList<>();
+		branchList.add(firstBranch);
+		List<BranchInfo> branchInfoList = new ArrayList<>();
+		branchInfoList.add(firstBranchInfo);
+		
+		when(branchRepository.findAll()).thenReturn(branchList);
+		when(branchMapper.mapToBranchInfoList(branchList)).thenReturn(branchInfoList);
+		
+		List<BranchInfo> returnList = branchService.getAllBranchInfo();
+		
+		assertEquals(branchInfoList, returnList);
 	}
 
 	@Test
 	void testGetBranchById() {
-		fail("Not yet implemented");
+		Long dummyId = 1L;
+		Branch firstBranch = makeBranchWithFirstRequest();
+		Optional<Branch> opBranch = Optional.of(firstBranch);
+		
+		when(branchRepository.findById(dummyId)).thenReturn(opBranch);
+		
+		Branch returnBranch = branchService.getBranchById(dummyId);
+		
+		assertEquals(firstBranch, returnBranch);
 	}
 
 	@Test
 	void testGetBranchInfoById() {
-		fail("Not yet implemented");
+		Long dummyId = 1L;
+		Branch firstBranch = makeBranchWithFirstRequest();
+		Optional<Branch> opBranch = Optional.of(firstBranch);
+		BranchInfo firstBranchInfo = makeBranchInfo(firstBranch);
+		
+		when(branchRepository.findById(dummyId)).thenReturn(opBranch);
+		when(branchMapper.mapToBranchInfo(firstBranch)).thenReturn(firstBranchInfo);
+		
+		BranchInfo returnBranchInfo = branchService.getBranchInfoById(dummyId);
+		
+		assertEquals(firstBranch, returnBranchInfo);
 	}
 
-	@Test
-	void testBranchService() {
-		fail("Not yet implemented");
+	
+	private BranchRequestBody firstRequestBody() {
+		BranchRequestBody requestBody = new BranchRequestBody();
+		requestBody.setName("firstName");
+		requestBody.setAddress("firstAddress");
+		requestBody.setPhoneNumber("firstPhoneNumber");
+		return requestBody;
+	}
+	
+	private Branch makeBranch(BranchRequestBody requestBody) {
+		Branch branch = new Branch();
+		branch.setName(requestBody.getName());
+		branch.setAddress(requestBody.getAddress());
+		branch.setPhoneNumber(requestBody.getPhoneNumber());
+		return branch;
+	}
+	
+	private Branch makeBranchWithFirstRequest() {
+		BranchRequestBody requestBody = firstRequestBody();
+		return makeBranch(requestBody);
+	}
+	
+	private BranchInfo makeBranchInfo(Branch branch) {
+		return BranchInfo.builder()
+				.branchId(1l)
+				.name(branch.getName())
+				.address(branch.getAddress())
+				.phoneNumber(branch.getPhoneNumber())
+				.build();
 	}
 
 }
